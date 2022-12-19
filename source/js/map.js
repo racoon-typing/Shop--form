dc443dfad14731b6b64aa99338d0478bf150bd78
Daniil <eo18622@yandex.ru>
2022-12-19 18:15:06 +0300

    карта

--- source/js/map.js
+++ source/js/map.js
@@ -1,22 +1,33 @@
 // DAData
-var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/address";
-var token = "0c6c9bedcb4d254c0775dbb3bbdf6371d4efe598";
-var query = "9120b43f-2fae-4838-a144-85e43c2bfb29";
 
-var options = {
-    method: "POST",
-    mode: "cors",
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Authorization": "Token " + token
-    },
-    body: JSON.stringify({query: query})
+let adressInput = document.querySelector('.form__input-address');
+adressInput.addEventListener('input', updateValue);
+let valueAdress;
+
+function updateValue(e) {
+    valueAdress = e.target.value;
+    mapAdress();
 }
 
-fetch(url, options)
-    .then(response => response.text())
-    .then(result => {
-        console.log(result);  
-    })
-    .catch(error => console.log("error", error));
+function mapAdress() {
+    var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address";
+    var token = "0c6c9bedcb4d254c0775dbb3bbdf6371d4efe598";
+    var query = `${valueAdress}`;
+    console.log(query);
+    
+    var options = {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": "Token " + token
+        },
+        body: JSON.stringify({query: query})
+    }
+    
+    fetch(url, options)
+        .then(response => response.json())
+        .then(result => console.log(result))
+        .catch(error => console.log("error", error));
+}
