af3cff8bb81eeb20fdf58739db2f5b975ccc4e6a
Daniil <eo18622@yandex.ru>
2022-12-19 09:02:31 +0300

    карта

---
+++ dist/js/map.js
@@ -0,0 +1,19 @@
+var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/address";
+var token = "0c6c9bedcb4d254c0775dbb3bbdf6371d4efe598";
+var query = "9120b43f-2fae-4838-a144-85e43c2bfb29";
+
+var options = {
+    method: "POST",
+    mode: "cors",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": "Token " + token
+    },
+    body: JSON.stringify({query: query})
+}
+
+fetch(url, options)
+.then(response => response.text())
+.then(result => console.log(result))
+.catch(error => console.log("error", error));
