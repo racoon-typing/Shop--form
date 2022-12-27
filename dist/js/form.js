302ce4ecd948c6cea06c1227a22d2c5cb4925732
Daniil <eo18622@yandex.ru>
2022-12-27 14:43:18 +0300

    phpmailer

--- dist/js/form.js
+++ dist/js/form.js
@@ -4,22 +4,43 @@ const telNode = form.querySelector('input[type="tel"]');
 const inputMask = new Inputmask('+7 (999) 999-99-99');
 inputMask.mask(telNode);
 
+
 // Валидация
 new window.JustValidate('.form', {
     rules: {
-        // name: {
-
-        // },
-        // email: {
-
-        // },
-        tel: {
+        surname: {
+            required: true,   
+        },
+        phone: {
             required: true,
             function: () => {
                 const phone = telNode.inputmask.unmaskedvalue();
                 return Number(phone) && phone.length === 10;
             }
+        },
+        adress: {
+            required: true,
         }
+    },
+    messages: {
+        name: {
+            required: 'Введите имя',
+            minLength: 'Введите 3 и более символов',
+            maxLength: 'Запрещено вводить более 15 символов',
+        },
+        surname: {
+            required: 'Введите фамилию',
+            minLength: 'Фамилия должно быть больше 5 букв'
+        },
+        phone: {
+            required: 'Введите телефон',
+            function: 'Здесь должно быть 10 символов, без +7'
+        },
+        email: {
+            email: 'Введите корректный email',
+            required: 'Введите email',
+        }, 
+        adress: 'Введите адрес',
     },
     submitHandler: function(thisForm) {
 
